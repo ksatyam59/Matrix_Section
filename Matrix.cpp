c75412e2c8f5d947ca25b9d3a5512f1e6f608482
Satyam Siddhant <50100965+ksatyam59@users.noreply.github.com>
2019-10-13 14:52:36 +0530

    Add files via upload

---
+++ Matrix.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+    int M[10][10];
+    int size=3;
+  // to take value in matrix
+    for(int i=0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+             cin>>M[i][j];
+    }
+  // to print value of matrix
+     for(int i=0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+
+            if(i==j)
+        {
+             cout<<M[i][j]<<" ";
+        }
+             cout<<endl;
+    }
+
+    return 0;
+}
